a77478c48461ab4e6624b84c92d2992b487e7238
Valmerson Silva <valmerson.silva@gmail.com>
2024-03-20 22:37:26 -0300

    MAIS UMA

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ def index():
 def teste():
   return "Esse é apenas um </b>teste</b> com o qual checamos a implementação de alterações no site" 
 
-@app.route("/telegram", methods=['POST'])
+@app.route("/telegram", methods=["POST"])
 def telegram_update():
   update=request.json
-  url_envio_mensagem=f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+  url_envio_mensagem=f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
   chat_id=update["mensage"] ["chat"] ["id"]
   mensagem={"chat_id": chat_id, "text": "mensagem <b>recebida</b>", "parse_mode":"HTML"}
   requests.post(url_envio_mensagem, data=mensagem)
