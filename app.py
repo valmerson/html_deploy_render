e6da4c91d2d14d22b8f6ad5aebabdfacce0f41af
Valmerson Silva <valmerson.silva@gmail.com>
2024-03-20 22:21:17 -0300

    ajustes_telegram

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request
+import requests 
+import os
 
 app = Flask(__name__)
+BOT_TOKEN=os.environ["TELEGRAM_BOT_TOKEN"]
 
 @app.route("/")
 def index():
@@ -10,5 +13,12 @@ def index():
 def teste():
   return "Esse é apenas um </b>teste</b> com o qual checamos a implementação de alterações no site" 
 
-
+@app.route("/telegram", methods=['POST'])
+def telegram_update():
+  update=request.json
+  url_envio_mensagem=f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+  chat_id=update["mensage"] ["chat"] ["id"]
+  mensagem={"chat_id": chat_id, "text": "mensagem <b>recebida</b>", "parse_mode":"HTML"}
+  requests.post(url_envio_mensagem, data=mensagem)
+  return "ok"
 
